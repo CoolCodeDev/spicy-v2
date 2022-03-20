457ad8c81a7a9637229224f95e2bec5f6a76fe8f
Johnny Nilsson <johnny@coolcode.se>
2022-03-20 10:55:17 +0100

    feature: create initial thread factory that names threads and inherits inheritable thread locals

---
+++ concurrency/src/main/java/se/coolcode/spicy/concurrency/NamedThreadFactory.java
@@ -0,0 +1,26 @@
+package se.coolcode.spicy.concurrency;
+
+import java.lang.management.MemoryMXBean;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class NamedThreadFactory implements ThreadFactory {
+    private final AtomicInteger count = new AtomicInteger();
+    private final String name;
+    private final ThreadGroup parentThreadGroup;
+
+    public NamedThreadFactory(String name) {
+        this(name, Thread.currentThread().getThreadGroup());
+    }
+
+    public NamedThreadFactory(String name, ThreadGroup parentThreadGroup) {
+        this.name = name;
+        this.parentThreadGroup = parentThreadGroup;
+    }
+
+    @Override
+    public Thread newThread(Runnable runnable) {
+        String threadName = String.format("%s-thread-%d", name, count.incrementAndGet());
+        return new Thread(parentThreadGroup, runnable, threadName, 0, true);
+    }
+}
