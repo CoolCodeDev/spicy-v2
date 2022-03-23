0b6e9a2a8e5952ce2c23fb39c98fd5e1ac71be29
Johnny Nilsson <johnny@coolcode.se>
2022-03-23 09:59:18 +0100

    feature: add methods for convenient creating strings of flat json structure

--- json/src/main/java/se/coolcode/spicy/json/Json.java
+++ json/src/main/java/se/coolcode/spicy/json/Json.java
@@ -1,10 +1,20 @@
 package se.coolcode.spicy.json;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class Json {
+
+    /**
+     * Matches a key value pair and groups the key and the value to referable groups. <br>
+     * Key is found in first group, value in the second group. <br>
+     * A regex group is denoted with ().
+     */
+    public static final String JSON_KEY_VALUE_REGEX = "\"(.*?)\": \"(.*?)\"";
 
     public static String toJson(Map<String, String> map) {
         return map == null ? null : map.entrySet().stream()
@@ -22,6 +32,16 @@ public class Json {
         return collection == null ? null : collection.stream()
                 .map(string -> String.format("\"%s\"", string))
                 .collect(Collectors.joining(", ", "[", "]"));
+    }
+
+    public static Map<String, String> toMap(String json) {
+        Pattern pattern = Pattern.compile(JSON_KEY_VALUE_REGEX, Pattern.MULTILINE);
+        Matcher matcher = pattern.matcher(json);
+        return matcher.results()
+                .collect(Collectors.toMap(
+                        match -> match.group(1),
+                        match -> match.group(2)));
+
     }
 
 }
